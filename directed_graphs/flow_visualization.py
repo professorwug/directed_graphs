a11c263c43002de06add06b85987cbf96f941617
Professor Wug <it@riddle.press>
2022-06-14 08:17:13 -0400

    Incorporated node2vec code; preliminary testing in progress

---
+++ directed_graphs/flow_visualization.py
@@ -0,0 +1,20 @@
+#
+#
+import torch # differentiable computing
+import numpy as np # matrix math
+import graph-tool # compute shortest path distances on graph
+import matplotlib.pyplot as plt # for visualization
+
+class Flow_artist:
+	def __init__(self, 
+		graph,
+		):
+		self.graph = graph
+		# compute distances
+	def compute_distance(self):
+		#
+		graph = self.graph
+
+	def flow_mds(self):
+	def visualize(self):
+
